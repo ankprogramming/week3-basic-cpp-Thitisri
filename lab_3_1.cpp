75eb082892d477e2b5fcdbbbffe8227129828de7
Unknown <Por.Thitisri@gmail.com>
2017-11-20 03:10:14 +0700

    แก้ข้อ1

--- lab_3_1.cpp
+++ lab_3_1.cpp
@@ -4,6 +4,12 @@ using namespace std;
 
 int main() {
   // เขียนโปรแกรมเพื่อแสดงผลตามที่กำหนด
-  cout << "Mr. Anukul" << endl;
+  cout << "Miss.Thitisri Butsang"
+  "Nickname:Por"
+  "Sex:Female"
+  "Born:08 July 1998"
+  "Hobbies:play facebook"
+  "Status:I want to visit the park with friends"
+  << endl;
   return 0;
 }
